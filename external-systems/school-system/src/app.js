70e3067a6fc4025d149d77ceb6794da9de0278d1
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2025-05-08 22:42:30 -0700

    Dockerizacion completada

--- external-systems/school-system/src/app.js
+++ external-systems/school-system/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const { SERVER_PORT: PORT } = require('./config/constants');
+const { SERVER_HOST: HOST, SERVER_PORT: PORT} = require('./config/constants');
 /**
     const gradesRouter = require('./routes/grades');
     const violationsRouter = require('./routes/violations');
@@ -17,10 +17,10 @@ app.use('/api/school-system', systemRouter);
 
 // Basic route
 app.get('/', (req, res) => {
-    res.send(`Welcome to the School System API! call http://localhost:${PORT}/api/school-system/info to get system information.`);
+    res.send(`Welcome to the School System API! call http://${HOST}:${PORT}/api/school-system/info to get system information.`);
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
 });
